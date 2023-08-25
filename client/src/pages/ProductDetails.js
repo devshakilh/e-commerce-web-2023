d43b73b3c8a3ce44ccf2bd936963b0a9762163ac
Md Shakil Hossain <108464347+shakilla1@users.noreply.github.com>
2023-08-25 16:26:00 +0600

    'update'

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -3,9 +3,12 @@ import Layout from "./../components/Layout/Layout";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 import "../styles/ProductDetailsStles.css";
+import { toast } from "react-hot-toast";
+import { useCart } from "../context/cart";
 
 const ProductDetails = () => {
   const params = useParams();
+  const [cart, setCart] = useCart();
   const navigate = useNavigate();
   const [product, setProduct] = useState({});
   const [relatedProducts, setRelatedProducts] = useState([]);
@@ -18,7 +21,7 @@ const ProductDetails = () => {
   const getProduct = async () => {
     try {
       const { data } = await axios.get(
-        `/api/v1/product/get-product/${params.slug}`
+        `https://ecommerceapp-psi.vercel.app/api/v1/product/get-product/${params.slug}`
       );
       setProduct(data?.product);
       getSimilarProduct(data?.product._id, data?.product.category._id);
@@ -30,7 +33,7 @@ const ProductDetails = () => {
   const getSimilarProduct = async (pid, cid) => {
     try {
       const { data } = await axios.get(
-        `/api/v1/product/related-product/${pid}/${cid}`
+        `https://ecommerceapp-psi.vercel.app/api/v1/product/related-product/${pid}/${cid}`
       );
       setRelatedProducts(data?.products);
     } catch (error) {
@@ -42,7 +45,7 @@ const ProductDetails = () => {
       <div className="row container product-details">
         <div className="col-md-6">
           <img
-            src={`/api/v1/product/product-photo/${product._id}`}
+            src={`https://ecommerceapp-psi.vercel.app/api/v1/product/product-photo/${product._id}`}
             className="card-img-top"
             alt={product.name}
             height="350"
@@ -63,10 +66,19 @@ const ProductDetails = () => {
             })}
           </h6>
           <h6>Category : {product?.category?.name}</h6>
-          <button class="btn btn-secondary ms-1 "
-          
-          
-          >ADD TO CART</button>
+          <button
+                  className="btn btn-dark ms-1"
+                  onClick={() => {
+                    setCart([...cart]);
+                    localStorage.setItem(
+                      "cart",
+                      JSON.stringify([...cart,product?.name])
+                    );
+                    toast.success("Item Added to cart");
+                  }}
+                >
+                  ADD TO CART
+                </button>
         </div>
       </div>
       <hr />
@@ -79,7 +91,7 @@ const ProductDetails = () => {
           {relatedProducts?.map((p) => (
             <div className="card m-2" key={p._id}>
               <img
-                src={`/api/v1/product/product-photo/${p._id}`}
+                src={`https://ecommerceapp-psi.vercel.app/api/v1/product/product-photo/${p._id}`}
                 className="card-img-top"
                 alt={p.name}
               />
@@ -103,7 +115,7 @@ const ProductDetails = () => {
                   >
                     More Details
                   </button>
-                  {/* <button
+                  <button
                   className="btn btn-dark ms-1"
                   onClick={() => {
                     setCart([...cart, p]);
@@ -115,7 +127,7 @@ const ProductDetails = () => {
                   }}
                 >
                   ADD TO CART
-                </button> */}
+                </button>
                 </div>
               </div>
             </div>
